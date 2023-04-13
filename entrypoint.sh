b34aa5643a70f7e36b7030d083954f4da3e944af
KoLiBer <koliberr136a1@gmail.com>
2023-04-13 11:34:57 +0330

    fix: set REDISCLI_AUTH if REDIS_PASS is not empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# export REDISCLI_AUTH=$REDIS_PASS
+[ -n "$REDIS_PASS" ] && export REDISCLI_AUTH=$REDIS_PASS
 
 redis-cli -h ${REDIS_HOST:-127.0.0.1} -p ${REDIS_PORT:-6379} --raw EVAL "$(cat /scripts/$1.lua)" 0 $2
